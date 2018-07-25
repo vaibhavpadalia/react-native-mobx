0503cbe4636be7647136cb3a11be7121157045fb
Vaibhav Padalia <vaibhavpadalia07@gmail.com>
2018-07-25 16:30:05 +0530

    Navigation initialized

--- App.js
+++ App.js
@@ -1,57 +1,14 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * @author Vaibhav Padalia
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { createStackNavigator } from "react-navigation";
+import Component1 from "./components/Component1";
+import Component2 from "./components/Component2";
+import Component3 from "./components/Component3";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default (App = createStackNavigator({
+  Component1: { screen: Component1 },
+  Component2: { screen: Component2 },
+  Component3: { screen: Component3 }
+}));
