5d93bdddc777d64c19cd7f6dde81d1c3f4d71161
Vaibhav Padalia <vaibhavpadalia07@gmail.com>
2018-07-27 18:35:19 +0530

    Comments added

--- components/Store.js
+++ components/Store.js
@@ -1,8 +1,14 @@
 import { observable, action } from "mobx";
-import { observer } from "mobx-react";
-
+/**
+ * @param userName the value that will change in different components.
+ * @description This is a class that contains store.
+ */
 class Store {
   @observable userName = "Vaibhav";
+
+  /**
+   * @description This function will modify the value of userName
+   */
 
   @action
   changeName = name => {
@@ -10,6 +16,7 @@ class Store {
   };
 }
 
+// Create object of the store and then import it in different components.
 const store = new Store();
 
 export default store;
